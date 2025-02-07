def03a7b0664bc09d2de5203731bd4b5b75c8ce3
Student Omkar <omkarbalwade@gmail.com>
2025-02-07 15:50:34 +0530

    first jdbc program

---
+++ JDBC programming/JDBCTutorials/src/com/jsp/ToLoadDriverClass.java
@@ -0,0 +1,42 @@
+package com.jsp;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ToLoadDriverClass 
+{
+
+	public static void main(String[] args) 
+	{
+		try 
+		{
+			String url = "jdbc:postgresql://localhost:5432/shop";
+			String un="postgres";
+			String password="root";
+			//step 1 to load Driver class
+			Class.forName("org.postgresql.Driver");
+			Connection conn=DriverManager.getConnection(url,un,password);
+			
+			String sql="insert into product values (2,'Radio',200)";
+			
+			Statement stmt = conn.createStatement();
+			stmt.execute(sql);
+			System.out.println(stmt.getUpdateCount()+" row inserted");
+			
+			
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
